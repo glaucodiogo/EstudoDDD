d47cecd388215e810df6791b995f89b8a60cf1a1
Glauco Diogo Alves de Oliveira <glaucodiogoalves@gmail.com>
2019-01-29 23:44:05 -0200

    base services

---
+++ Service/Validators/UsuarioValidator.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Service.Validators
+{
+    public class UsuarioValidator : AbstractValidator<User>
+    {
+        public UsuarioValidator()
+        {
+            RuleFor(c => c)
+                .NotNull()
+                .OnAnyFailure(x =>
+                {
+                    throw new System.ArgumentNullException("Can't found the object.");
+                });
+
+            RuleFor(c => c.Cpf)
+                .NotEmpty().WithMessage("Informe seu CPF.")
+                .NotNull().WithMessage("Informe seu CPF.");
+
+            RuleFor(c => c.Aniversario)
+                .NotEmpty().WithMessage("Informe sua data de aniversário.")
+                .NotNull().WithMessage("Informe sua data de aniversário");
+
+            RuleFor(c => c.Nome)
+                .NotEmpty().WithMessage("Informe seu Nome.")
+                .NotNull().WithMessage("Informe seu Nome.");
+        }
+
+    }
+}
