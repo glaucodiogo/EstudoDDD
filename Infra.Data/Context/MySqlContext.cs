f1632ec6990427adba9d059aa580ea28ad11dcdf
Glauco Diogo Alves de Oliveira <glaucodiogoalves@gmail.com>
2019-01-29 23:45:40 -0200

    base infra data

---
+++ Infra.Data/Context/MySqlContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infra.Data.Context
+{
+    public class MySqlContext:DbContext
+    {
+        public DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseMySql("Server=localhost;Port=3306;Database=EstudoDDD;Uid=root;Pwd=1234");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Usuario>(new UserMap().Configure);
+        }
+    }
+}
---
+++ Infra.Data/Mapping/UserMap.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infra.Data.Mapping
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Cpf)
+                .IsRequired()
+                .HasColumnName("Cpf");
+
+            builder.Property(c => c.Aniversario)
+                .IsRequired()
+                .HasColumnName("Aniversario");
+
+
+            builder.Property(c => c.Nome)
+                .IsRequired()
+                .HasColumnName("Nome");
+        }
+    }
+}
