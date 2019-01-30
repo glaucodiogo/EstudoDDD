3af75801dbffe6cb6e096f03f96d10dddb0ed585
Glauco Diogo Alves de Oliveira <glaucodiogoalves@gmail.com>
2019-01-29 23:44:54 -0200

    base repositorio

---
+++ Repositorio/IEnderecoRepository.cs
@@ -0,0 +1,12 @@
+﻿using Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositorio
+{
+    public interface IEnderecoRepository : IRepository<Endereco>
+    {
+        IEnumerable<Endereco> ObterEnderecoPorMorador(Pessoa morador);
+    }
+}
---
+++ Repositorio/IPessoaRepository.cs
@@ -0,0 +1,12 @@
+﻿using Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositorio
+{
+    public interface IPessoaRepository :IRepository<Pessoa>
+    {
+        IEnumerable<Pessoa> ObterMoradoresPorEndereco(Endereco endereco);
+    }
+}
