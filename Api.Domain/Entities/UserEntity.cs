6a793882ee27bb83892a0afeb97b4fa69787620e
Glauco Diogo Alves de Oliveira <glaucodiogoalves@gmail.com>
2019-11-20 23:05:19 -0300

    Criando entidades que devem pertencer a um domínio

---
+++ Api.Domain/Entities/UserEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Domain.Entities
+{
+    public class UserEntity : BaseEntity
+    {
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ Api.Domain/ReadMe.txt
@@ -0,0 +1,2 @@
+﻿A pasta entities deve conter a representação das entidades de banco de dados .
+Algumas tabelas contém  campos em comum, sendo necessario usar baseentity
