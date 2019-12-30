2f46033c3da13a15aadaaf50d06fbe1c53f96030
Glauco Diogo Alves de Oliveira <glaucodiogoalves@gmail.com>
2019-12-29 23:32:05 -0300

    Utilizando view components para garantir melhor performance e processamento assincrono, caso eu tenha uma parte da view que utilize consulta em dados. Um exemplo de uso de view components seria o carrinho de um ecommerce.

---
+++ Mvc.Application/ViewComponents/CarrinhoViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mvc.Application.ViewComponents
+{
+    [ViewComponent(Name = "Carrinho")]
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        public int ItensCarrinho { get; set; }
+
+        public CarrinhoViewComponent()
+        {
+            ItensCarrinho = 3;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(ItensCarrinho);
+        }
+    }
+}
