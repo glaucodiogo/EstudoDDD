1f5f6143df729d0d00d6bf2bea52615e99f82b29
Glauco Diogo Alves de Oliveira <glaucodiogoalves@gmail.com>
2019-12-30 01:24:05 -0300

    Validações de formulário construido com tag helper e sendo validado pelo model filme. Adicionado Section scripts ,invocando o jquery validation para que as validações ocorram diretamente no client (navegador) e não direto server side (chamando o servidor toda hora)

---
+++ Mvc.Application/Controllers/FilmesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mvc.Application.Models;
+
+namespace Mvc.Application.Controllers
+{
+    public class FilmesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Adicionar(Filme filme)
+        {
+            if (ModelState.IsValid)
+            {
+                //verificar depois 
+            }
+            return View(filme);
+        }
+    }
+}
